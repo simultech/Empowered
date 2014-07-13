7e7d3c4d38774d4859d9144902cb3ae89fc6c3d2
paul6325106 <paul6325106@gmail.com>
2014-07-13 12:22:20 +1000

    getPaymentsReceived(stage, age) methods

--- app/Controller/AwarenessController.php
+++ app/Controller/AwarenessController.php
@@ -2,9 +2,71 @@
 class AwarenessController extends AppController {
 
 	public function index() {
-		$data = "18";
+		$data = getPaymentsReceived($state, $age, "Carer Allowance"
 		$this->set('data',$data);
 	}
-
+	
+	function getCarerAllowancePaymentsReceived($state, $age, $gender) {
+		return getPaymentsReceived($state, $age, "Carer Allowance");
+	}
+	
+	function getCarerAllowanceHCCOnlyPaymentsReceived($state, $age, $gender) {
+		return getPaymentsReceived($state, $age, "Carer Allowance (child hcc only)");
+	}
+	
+	function getDisabilitySupportPensionPaymentsReceived($state, $age, $gender) {
+		return getPaymentsReceived($state, $age, "Disability Support Pension");
+	}
+	
+	function getPaymentsReceived($state, $age, $field) {
+		//field is one of "Carer Allowance", "Carer Allowance (child hcc only)", "Carer Payment", "Disability Support Pension"
+		
+		if ($age < 24) {
+			if ($field = "Disability Support Pension") {
+				$index = 2;
+			} else {
+				$index = 3;
+			}
+		} else if ($age < 35) {
+			$index = 4;
+		} else if ($age < 44) {
+			$index = 5;
+		} else if ($age < 54) {
+			$index = 6;
+		} else if ($age < 64) {
+			$index = 7;
+		} else {
+			$index = 8;
+		}
+		if ($state == "ACT") {
+			//do nothing lol
+		} else if ($state == "NSW") {
+			$index += 9;
+		} else if ($state == "NT") {
+			$index += 18;
+		} else if ($state == "QLD") {
+			$index += 27;
+		} else if ($state == "SA") {
+			$index += 36;
+		} else if ($state = "TAS") {
+			$index += 45;
+		} else if ($state == "VIC") {
+			$index += 54;
+		} else { //if $state == "WA"
+			$index += 63;
+		}
+		
+		$header = NULL;
+		if (($handle = fopen("files/march2104paymentrecipientsbypaymenttypebystateandterritorybyagegroup.csv", 'r')) !== FALSE) {
+			while (($row = fgetcsv($handle, 1000, ',')) !== FALSE) {
+				if(!$header) {
+					$header = $row;
+				} else if ($row[0] == $field) {
+					fclose($handle);
+					return $row[$index];
+				}
+			}
+		}
+	}
 }
 ?>
